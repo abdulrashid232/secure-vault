b1bbf8546a2340fca6a1edf3c780dc9034215db5
Abdel <abdulrashidissah232@gmail.com>
2026-02-13 10:06:16 +0000

    feat: add initial routing for design system demo with root path redirect

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'design-system',
+        pathMatch: 'full'
+    },
+    {
+        path: 'design-system',
+        loadComponent: () => import('./pages/design-system-demo/design-system-demo').then(m => m.DesignSystemDemoComponent)
+    },
+];
